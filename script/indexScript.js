8d009c1706d32023e350f02f80863f928e97f951
spetrov1 <petrov165@abv.bg>
2020-02-03 21:18:33 +0200

    login and registration form working

--- script/indexScript.js
+++ script/indexScript.js
@@ -1,25 +1,20 @@
-var inputs = document.getElementsByClassName("input");
 
-/*
-function loginFunction() {
-    const registerForm = document.getElementById('login-form');
-    registerForm.addEventListener('submit', event => {
-        const formData = new FormData(event.target);
+const loginForm = document.getElementById('login-form');
 
-        const email = formData.get('email');
-        const password = formData.get('password');
+loginForm.addEventListener('submit', event => {
+	const formData = new FormData(event.target);
+	
+	const email = formData.get('email');
+	const password = formData.get('password');
 
-        event.preventDefault();
+	event.preventDefault();
 
-        auth.login(email, password, (success, errorCode, errorMessage) => {
-            if (success) {
-                window.location = 'posts.html';
-            } else {
-                errors.classList.add('errors-visible');
-                errors.innerText = errorMessage;
-            }
-        });
-    });
-}
-
-*/
+	login(email, password, (success, errorCode, errorMessage) => {
+		if (success) {
+			alert("Successfully loged in your acc!");
+			window.location = 'main.html';
+		} else {
+			document.getElementById("validation").innerHTML = "" + errorMessage;
+		}
+	});
+});
