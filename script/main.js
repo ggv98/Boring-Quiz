36af875a47277537341548b8748af986c2fbe0f5
Georgi Vankov <ggb98@abv.bg>
2020-02-04 12:23:47 +0200

    add authentication to all pages

---
+++ script/main.js
@@ -0,0 +1,14 @@
+let usernameElem = document.getElementById("page-header-username");
+(function(){
+    firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      usernameElem.innerText = "Hello" + user.displayName;
+    } else {
+      window.location = "index.html";
+    }
+  });
+})();
+const exit = () =>{
+   console.log( logout());
+    window.location = "index.html";
+} 
